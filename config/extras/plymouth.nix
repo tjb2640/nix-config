dcb533da91b096257e487dacc1fd4967e7ef30e8
tjb2640 <95368581+tjb2640@users.noreply.github.com>
2025-12-25 20:44:13 -0500

    Restore params meant to make booting quieter

--- config/extras/plymouth.nix
+++ config/extras/plymouth.nix
@@ -21,13 +21,13 @@ in
     # other plymouth-related things
     consoleLogLevel = 3;
     initrd.verbose = false;
-#    kernelParams = [
-#      "quiet"
-#      "splash"
-#      "boot.shell_on_fail"
-#      "udev.log_priority=3"
-#      "rd.systemd.show_status=auto"
-#    ];
-#    loader.timeout = 0;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+    loader.timeout = 0;
   };
 }
