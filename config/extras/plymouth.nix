188b749cf51ced313d33c8830aaa6e297260c45a
tjb2640 <95368581+tjb2640@users.noreply.github.com>
2025-12-25 20:43:28 -0500

    Use rbsys plymouth theme (custom) as plymouth theme

---
+++ config/extras/plymouth.nix
@@ -0,0 +1,33 @@
+{ input, pkgs, libs, ... }:
+
+# Testing out plymouth theming 12/25
+
+let 
+  plymouth-theme-rbsys = pkgs.callPackage ../../overlays/system/plymouth/plymouth-theme-rbsys/default.nix {};
+in
+{
+
+  users.users.ty.packages = with pkgs; [
+    plymouth-theme-rbsys
+  ];
+
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "rbsys";
+      themePackages = [ plymouth-theme-rbsys ];
+    };
+
+    # other plymouth-related things
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+#    kernelParams = [
+#      "quiet"
+#      "splash"
+#      "boot.shell_on_fail"
+#      "udev.log_priority=3"
+#      "rd.systemd.show_status=auto"
+#    ];
+#    loader.timeout = 0;
+  };
+}
---
+++ overlays/system/plymouth/plymouth-theme-rbsys/default.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, lib, stdenv, fetchurl, unzip, ... }:
+
+# Theme plymouth with an "rbsys" logo featuring my window decorations
+
+stdenv.mkDerivation rec {
+  pname = "plymouth-theme-rbsys";
+  version = "2025.12.25.0";
+
+  rbsysTheme = fetchurl {
+    url = "https://github.com/tjb2640/plymouth-theme-rbsys/archive/refs/tags/v1.0.0.zip";
+    sha256 = "sha256-bw22+PhWYyC0xHsYU5+7gN9cqeQae+7qO/ADAFyYPzU=";
+  };
+
+  nativeBuildInputs = [ unzip ];
+  phases = [ "unpackPhase" "installPhase" ];
+
+  unpackPhase = ''
+    unzip ${rbsysTheme}
+    mkdir -p $out/plymouthfiles
+    mv plymouth-theme-rbsys-1.0.0/* $out/plymouthfiles/
+  '';
+
+  installPhase = ''
+    mkdir -p $out/share/plymouth/themes/rbsys
+    install -m 755 -vDt "$out/share/plymouth/themes/rbsys" "$out/plymouthfiles/rbsys."{plymouth,script}
+    install -m 644 -vDt "$out/share/plymouth/themes/rbsys" "$out/plymouthfiles/"*png
+    sed -i "s@\/usr\/@$out\/@" "$out/share/plymouth/themes/rbsys/rbsys.plymouth"
+  '';
+
+  meta = {
+    description = "rbsys Plymouth Theme";
+    homepage = "https://github.com/tjb2640/plymouth-theme-rbsys/";
+    license = lib.licenses.mit;
+  };
+
+}
