bc1b0b0bfe7219bf09609346179aeeb333cf3d5a
tjb2640 <95368581+tjb2640@users.noreply.github.com>
2026-01-15 10:46:28 -0500

    Add an extras file for zoom, in case I need to tweak something

---
+++ config/system/extras/zoom.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, lib, ... }:
+
+# zoom virtual meeting software
+
+{
+
+  programs.zoom-us.enable = true;
+
+}
--- hosts/nix13/configuration.nix
+++ hosts/nix13/configuration.nix
@@ -12,6 +12,7 @@
     ../../config/system/extras/firefox.nix
     ../../config/system/extras/gaming.nix
     ../../config/system/extras/plymouth.nix
+    ../../config/system/extras/zoom.nix
   ];
 
   boot.initrd.luks.devices."luks-fa0277a9-062a-4b4c-b011-092fc1815abf".device = "/dev/disk/by-uuid/fa0277a9-062a-4b4c-b011-092fc1815abf";
