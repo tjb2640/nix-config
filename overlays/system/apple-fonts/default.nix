14c7ae412058d3e3b6410e1a1ab5ca68646059ff
tjb2640 <95368581+tjb2640@users.noreply.github.com>
2026-01-17 19:24:51 -0500

    Looks like Apple's SF fonts updated

--- overlays/system/apple-fonts/default.nix
+++ overlays/system/apple-fonts/default.nix
@@ -8,12 +8,12 @@ stdenv.mkDerivation rec {
 
   appleFontSFPro = fetchurl {
     url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
-    sha256 = "sha256-Lk14U5iLc03BrzO5IdjUwORADqwxKSSg6rS3OlH9aa4=";
+    sha256 = "sha256-u7cLbIRELSNFUa2OW/ZAgIu6vbmK/8kXXqU97xphA+0=";
   };
 
   appleFontSFCompact = fetchurl {
     url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
-    sha256 = "sha256-CMNP+sL5nshwK0lGBERp+S3YinscCGTi1LVZVl+PuOM=";
+    sha256 = "sha256-mcMM/cbmOA5ykyIb74bid9vU6wyl8nVwkvkd+VlOdwo=";
   };
 
   appleFontSFMono = fetchurl {
