78dc398f743eb712c8c77e17b0760796321e7c88
tjb2640 <95368581+tjb2640@users.noreply.github.com>
2026-02-11 18:27:46 -0500

    Update helper script for encrypted flash drives: add format option

--- config/home/users/ty/files/localbin/cryptdrive.sh
+++ config/home/users/ty/files/localbin/cryptdrive.sh
@@ -1,77 +1,154 @@
 #!/bin/sh
 
-luksdevice="myfd"
+luksdevicename="cryptdriveflash"
 username="$(whoami)"
-mountpoint="/mnt/cryptdrive"
-# links to $mountpoint/$username
+mountpoint="/mnt/__cryptdrive"
+userdrivedir="$mountpoint/$username"
+# links to $userdrivedir
 linkat="/home/$username/CryptDrive"
 
 if [ "$username" == "root" ]; then
-    printf "This script should not be run as root\n"
+    printf "Do not run this script with root permissions.\n"
     exit 1
 fi
 
-actionMountDrive() {
-    printf "====================================\n"
-    lsblk -Spyd --properties-by=file
-    printf "====================================\n"
+printbar() {
+    printf "=========================================\n"
+}
 
+step() {
+    printf -- "\n --> $1 ... \n"
+}
+
+confirmation=""
+promptConfirmAction() {
+    confirmation=""
+    printf "\n"
+    printbar
+    printf "Type 'i am sure' in ALL CAPS to continue.\n"
+    printbar
+    printf "\n"
+    read -p "Type here and hit enter: " confirmation
+    if [ "$confirmation" != "I AM SURE" ]; then
+        promptConfirmAction
+    fi
+}
+
+dpath=""
+promptSelectDevice() {
+    dpath=""
+    printf "\n"
+    printbar
+    printf "Please select the correct block.\n"
+    printbar
+
+    # A little more useful information here when retrieving by file
+    lsblk -Spyd --properties-by=file
+    printbar
     read -p "Enter mount source (e.g. /dev/sdX): " dpath
+
+    if [ "$dpath" == "" ]; then
+        printf "Please enter a valid block, e.g. /dev/sdX \n"
+        promptSelectDevice
+    fi
 
     lsblk -nd -o PTTYPE -- "$dpath" 1> /dev/null 2>/dev/null
     if [ $? -eq 32 ]; then
-        printf " --> $dpath is not a valid source\n"
-        exit 1
+        printf "'$dpath' is not a valid block!\n"
+        promptSelectDevice
     fi
+}
+
+actionFormatDrive() {
+    printf "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+    printf "!! FAIR WARNING!!                                         !!\n"
+    printf "!! By continuing you understand that this will completely !!\n"
+    printf "!! wipe the data from the selected block and format the   !!\n"
+    printf "!! device entirely. This will overwrite all data on the   !!\n"
+    printf "!! selected device. This action is destructive.           !!\n"
+    printf "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+    promptConfirmAction
+    promptSelectDevice
+
+    # Format drive
+    printf "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+    printf "!! Please confirm this looks like the correct command+path !!\n"
+    printf "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+    printf "\n# cryptsetup luksFormat -- $dpath \n"
+    promptConfirmAction
+
+    step "luksFormat $dpath"
+    sudo cryptsetup luksFormat -- "$dpath"
+
+    step "luksOpen $dpath"
+    sudo cryptsetup luksOpen -- "$dpath" "$luksdevicename"
+
+    step "create EXT4 partition"
+    sudo mkfs.ext4 -- "/dev/mapper/$luksdevicename"
+
+    step "close LUKS device $luksdevicename"
+    sudo cryptsetup close -- "$luksdevicename"
+
+    printf "\nDone! Run this script with the -m flag to mount the device.\n"
+    exit 0
+}
+
+actionMountDrive() {
+    promptSelectDevice
 
     if ! [ -d "$mountpoint" ]; then
-        printf "Need to mkdir the mount point at $mountpoint --\n"
+        step "mkdir the mount point at $mountpoint"
         sudo mkdir -p -- "$mountpoint"
         if [ $? -ne 0 ]; then
             exit 1
         fi
     fi
 
-    printf "(1/3) Open the LUKS device\n"
-    sudo cryptsetup open -- "$dpath" "$luksdevice"
+    step "open the LUKS device"
+    sudo cryptsetup open -- "$dpath" "$luksdevicename"
 
-    printf "(2/3) Mount the encrypted partition\n"
-    sudo mount -- "/dev/mapper/$luksdevice" "$mountpoint"
+    step "mount the encrypted partition"
+    sudo mount -- "/dev/mapper/$luksdevicename" "$mountpoint"
     
-    userdrivedir="$mountpoint/$username"
-    printf "(3/3) Set/check permissions for subdir $userdrivedir \n"
+    step "set/check permissions for subdir $userdrivedir"
     sudo mkdir -p -- "$userdrivedir"
     sudo chown -- "$username" "$userdrivedir"
-    sudo chmod 700 "$userdrivedir"
+    sudo chmod 700 -- "$userdrivedir"
 
     if [ -L "$linkat" ]; then
-        unlink "$linkat"
+        unlink -- "$linkat"
     fi
-    ln -s "$userdrivedir" "$linkat"
+    ln -s -- "$userdrivedir" "$linkat"
 
-    printf "Done! @ $linkat \n"
+    printf "Done - mounted drive at $linkat \n"
     exit 0
 }
 
 actionUnmountDrive() {
-    printf "Unmounting $mountpoint ...\n"
+    step "unmount $mountpoint"
     sudo umount -- "$mountpoint"
-    printf "Closing device ...\n"
-    sudo cryptsetup close -- "$luksdevice"
-    printf "Cleaning up link ...\n"
-    unlink "$linkat"
+
+    if [ -L "$linkat" ]; then
+        step "clean up dead link at $linkat"
+        unlink -- "$linkat"
+    fi
+
+    step "close LUKS device '$luksdevicename'"
+    sudo cryptsetup close -- "$luksdevicename"
+
     printf "Done! You are free to remove the drive\n"
     exit 0
 }
 
-while getopts "mu" opt; do
+while getopts "fmu" opt; do
     case "$opt" in
+        f) actionFormatDrive ;;
         m) actionMountDrive ;;
-	u) actionUnmountDrive ;;
+        u) actionUnmountDrive ;;
     esac
 done
 
 printf "Usage:\n"
-printf " -m :: mount a drive\n"
-printf " -u :: unmount the mounted drive\n"
-
+printf " -f :: LUKS encrypt a drive and create an EXT4 partition inside it\n"
+printf " -m :: mount an encrypted drive\n"
+printf " -u :: unmount the mounted encrypted drive\n"
